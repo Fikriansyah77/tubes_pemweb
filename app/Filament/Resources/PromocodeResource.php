09b2654fa46283d4f2f8bc662603fc693134253a
Fikriansyah <fikriansyah@mhs.itenas.ac.id>
2025-01-11 21:50:47 +0700

    mengganti icon

--- app/Filament/Resources/PromocodeResource.php
+++ app/Filament/Resources/PromocodeResource.php
@@ -14,7 +14,7 @@ class PromocodeResource extends Resource
 {
     protected static ?string $model = Promocode::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
 
     public static function form(Form $form): Form
     {
