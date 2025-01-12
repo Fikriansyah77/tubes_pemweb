84b4c24098d969d433e8a024f120621e8e96c99b
Namamu <emailmu@example.com>
2025-01-12 16:20:37 +0700

    menyelesaikan flight

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
+use App\Http\Controllers\FlightController;
+use APP\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class,'index']); 
+Route::get('flight',[FlightController::class,'index'])->name('flight.index');
