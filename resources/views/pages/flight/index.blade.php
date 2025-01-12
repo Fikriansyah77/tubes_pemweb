8cd1cefc68081ace076dc02f67eaf226987a4222
Namamu <emailmu@example.com>
2025-01-12 16:37:54 +0700

    menyelesaikan flight 2

--- resources/views/pages/flight/index.blade.php
+++ resources/views/pages/flight/index.blade.php
@@ -2,3 +2,16 @@
 
 @section('content')
 @endsection
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>SELAMAT DATANG DI WEB KAMI</h1>
+</body>
+</html>
